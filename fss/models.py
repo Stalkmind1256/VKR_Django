51833c33f93b7a6854d79ca93a3bf13e14ceb1cc
andar <andar-shadrinsk@mail.ru>
2024-11-14 18:15:53 +0500

    add_db

--- fss/models.py
+++ fss/models.py
@@ -1,33 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# class User(models.Model):
-#     username = models.CharField(max_length=100)
-#     lastname = models.CharField(max_length=100)
-#     middlename = models.CharField(max_length=100)
-#     email = models.Email`Field()
-#     role = models.CharField(100)
-#     date_registr = models.DateTimeField()
 class Status(models.Model):
-    status_name = models.CharField(max_length=200)
+    STATUS_CHOICES = [
+        ('pending','Pending'),
+        ('approved','Approved'),
+        ('rejected','Rejected')
+    ]
+    status_name = models.CharField(max_length=200,choices=STATUS_CHOICES)
 
     def __str__(self):
         return self.status_name
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=100,unique=True)
+    CATEGORY_CHOICES = [
+        ('technology','Technology'),
+        ('education','Education'),
+    ]
+    category_name = models.CharField(max_length=100,choices=CATEGORY_CHOICES)
 
     def __str__(self):
         return self.category_name
 
 
 class Suggestion(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='suggestions')
-    # user_id = models.models.IntegerField(_(""))
+    user = models.ForeignKey(User,on_delete=models.PROTECT,related_name='suggestions')
     title = models.CharField(max_length=255)
     description = models.TextField()
-    category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True, related_name='suggestions')
-    status = models.ForeignKey(Status,on_delete=models.SET_NULL, null=True, related_name='suggestions')
+    category = models.ForeignKey(Category,on_delete=models.PROTECT, null=True, related_name='suggestions')
+    status = models.ForeignKey(Status,on_delete=models.PROTECT, null=True, related_name='suggestions')
     date_create = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
