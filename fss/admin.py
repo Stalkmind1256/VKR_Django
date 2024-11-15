12d107d2cf84c79c2f279fd0a46be0b8314695df
Stalkmind1256 <sterhov.andrej2016@yandex.ru>
2024-11-15 07:58:12 +0300

    registr_base

--- fss/admin.py
+++ fss/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Status, Category, Suggestion,Role
 
-# Register your models here.
+admin.site.register(Status)
+admin.site.register(Category)
+admin.site.register(Suggestion)
+admin.site.register(Role)
+
+
