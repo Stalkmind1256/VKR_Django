2a68b99bf41a4f7687f6811ed8127a6971bbf9d1
Stalkmind1256 <sterhov.andrej2016@yandex.ru>
2024-11-17 17:19:17 +0300

    add_newbranch

--- fss/urls.py
+++ fss/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.suggestion_list, name='suggestion_list'),
     path('suggestions/', views.suggestion_form, name='suggestion_form'),
+    path('suggestion/<int:pk>/', views.suggestion_detail, name='suggestion_detail'),
 ]
--- fss/views.py
+++ fss/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from unicodedata import category
+from .models import Suggestion
 
 def suggestion_list(request):
-    return render(request,'fss/suggestion_list.html',{})
+    suggestions = Suggestion.objects.select_related('category', 'status').all()
+
+    # Фильтрация по категориям и статусам
+    category = request.GET.get('category')
+    status = request.GET.get('status')
+
+    if category:
+        suggestions = suggestions.filter(category__name=category)
+    if status:
+        suggestions = suggestions.filter(status__name=status)
+
+    return render(request, 'fss/suggestion_list.html', {'suggestions': suggestions})
 
 def suggestion_form(request):
     return render(request,'fss/suggestion_form.html',{})
 
+def suggestion_detail(request, pk):
+    suggestion = get_object_or_404(Suggestion, pk=pk)
+    return render(request, 'fss/suggestion_detail.html', {'suggestion': suggestion})
