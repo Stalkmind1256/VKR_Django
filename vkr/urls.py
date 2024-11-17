d50a34f5d4b233fd97718327f9a5518f240b8840
Stalkmind1256 <sterhov.andrej2016@yandex.ru>
2024-11-17 15:38:31 +0300

    updte_db

--- vkr/urls.py
+++ vkr/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v0/', include('api_v0.urls'))
+    path('api/v0/', include('api_v0.urls')),
+    path('', include('fss.urls')),
+
 ]
