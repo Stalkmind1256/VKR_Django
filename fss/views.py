15a40143a83b228f3b42606fa53260c576bda156
Stalkmind1256 <sterhov.andrej2016@yandex.ru>
2024-11-17 17:31:39 +0300

    fix output categories and add time_filter

--- fss/views.py
+++ fss/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils.dateparse import parse_date
 from unicodedata import category
 from .models import Suggestion
 
@@ -8,11 +9,19 @@ def suggestion_list(request):
     # Фильтрация по категориям и статусам
     category = request.GET.get('category')
     status = request.GET.get('status')
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
 
     if category:
         suggestions = suggestions.filter(category__name=category)
+
     if status:
         suggestions = suggestions.filter(status__name=status)
+
+    if start_date:
+        suggestions = suggestions.filter(date_create__gte=parse_date(start_date))
+    if end_date:
+        suggestions = suggestions.filter(date_create__lte=parse_date(end_date))
 
     return render(request, 'fss/suggestion_list.html', {'suggestions': suggestions})
 
