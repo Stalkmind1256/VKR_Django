295fe9466a814fe68ec05510992a31b4c5581d5a
Stalkmind1256 <sterhov.andrej2016@yandex.ru>
2024-11-20 20:17:34 +0300

    ispravit formu login

--- fss/forms.py
+++ fss/forms.py
@@ -7,7 +7,7 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', '<PASSWORD>', '<PASSWORD>')
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class LoginForm(forms.Form):
