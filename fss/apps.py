b140a035e41a62682838210ee052717b58a81c6e
Stalkmind1256 <sterhov.andrej2016@yandex.ru>
2025-06-07 11:34:25 +0500

    commit

--- fss/apps.py
+++ fss/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FssConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fss'
+
+    def ready(self):
+        import fss.signals  # Импорт сигналов при старте
---
+++ fss/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Suggestion, Notification, CustomUser
+
+@receiver(post_save, sender=Suggestion)
+def notify_admin_on_suggestion_create(sender, instance, created, **kwargs):
+    if created:
+        admins = CustomUser.objects.filter(is_superuser=True)
+        for admin in admins:
+            Notification.objects.create(
+                user=admin,
+                message=f"Новое предложение: «{instance.title}» от {instance.user.get_full_name()}",
+            )
