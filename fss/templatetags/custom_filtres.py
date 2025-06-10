ac36e9bd50aa92b35e222db74911ce1c23f2ee1e
Stalkmind1256 <sterhov.andrej2016@yandex.ru>
2025-06-10 17:04:19 +0500

    shortname

---
+++ fss/templatetags/custom_filtres.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def initials(user):
+    if not user:
+        return ""
+    last = user.last_name or ""
+    first = user.first_name[0] + '.' if user.first_name else ""
+    patronymic = user.patronymic[0] + '.' if hasattr(user, 'patronymic') and user.patronymic else ""
+    return f"{last}.{first}{patronymic}"
