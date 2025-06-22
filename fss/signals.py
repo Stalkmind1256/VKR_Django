86bd9953f789fe1fe6a96c110b123dbaf6cc8a3a
Stalkmind1256 <sterhov.andrej2016@yandex.ru>
2025-06-22 12:55:18 +0300

    commit

--- fss/signals.py
+++ fss/signals.py
@@ -1,6 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Suggestion, Notification, CustomUser
+from .models import Suggestion, Notification, CustomUser, Role
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 @receiver(post_save, sender=Suggestion)
 def notify_admin_on_suggestion_create(sender, instance, created, **kwargs):
@@ -11,3 +15,6 @@ def notify_admin_on_suggestion_create(sender, instance, created, **kwargs):
                 user=admin,
                 message=f"Новое предложение: «{instance.title}» от {instance.user.get_full_name()}",
             )
+
+
+
